85cb785b83a8278a65883e3787e4db3fde74e7c1
ecmerchant <ecmerchant.contact@gmail.com>
2018-01-06 14:38:39 +0900

    fix csv output

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180104144952) do
+ActiveRecord::Schema.define(version: 20180104114933) do
 
   create_table "Products", force: :cascade do |t|
     t.string   "user"
